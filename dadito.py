4e972e787a626a2e025ec4d0ed9452402fc58c8d
diegosskii <di.ormazabal@duocuc.cl>
2022-08-11 19:26:59 -0400

    intento de python 2.0

--- dadito.py
+++ dadito.py
@@ -1,17 +1,25 @@
 import random
 print("hola mundo")
 
-dado = random.randrange(7) 
+variable = input("quieres jugar al dado s/n")
 
-if dado == 1:
-    print("1")
-if dado == 2:
-    print("2")
-if dado == 3:
-    print("3")
-if dado == 4:
-    print("4")
-if dado == 5:
-    print("5")
+if variable == "s":
+
+    dado = random.randrange(7) 
+
+    if dado == 1:
+        print("1")
+    if dado == 2:
+        print("2")
+    if dado == 3:
+        print("3")
+    if dado == 4:
+        print("4")
+    if dado == 5:
+        print("5")
+    else:
+        print("6")
+
 else:
-    print("6")
+    print (".I.")
+
