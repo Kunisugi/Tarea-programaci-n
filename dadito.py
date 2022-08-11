594c211a7c5da2e2fa3c69f41b0dadec6faa55ea
Lucas Cortes <luc.cortes@duocuc.cl>
2022-08-11 19:21:46 -0400

    agrego dadito.py para que no me borren

---
+++ dadito.py
@@ -0,0 +1,17 @@
+import random
+print("hola mundo")
+
+dado = random.randrange(7) 
+
+if dado == 1:
+    print("1")
+if dado == 2:
+    print("2")
+if dado == 3:
+    print("3")
+if dado == 4:
+    print("4")
+if dado == 5:
+    print("5")
+else:
+    print("6")
--- python.py
+++ python.py
@@ -1,12 +1,5 @@
 
-import random
 print("hola mundo")
 
-dado = random.randrange(7) 
-
-if dado == 1:
-    print("lo mas bajo")
-if dado == 6:
-    print("lo mas alto")
-else:
-    print("intermedio")
+variable = input("ingresar un texto")
+print (variable)
